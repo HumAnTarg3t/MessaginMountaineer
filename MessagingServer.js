d63292bbd441853347669360d4fbb38c8e8b97c9
N.Tsa <24270865+HumAnTarg3t@users.noreply.github.com>
2022-12-12 23:30:20 +0100

    Css, begynt på direkte message

--- MessagingServer.js
+++ MessagingServer.js
@@ -1,15 +1,17 @@
+const express = require("express");
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
-
+app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/MessaginClient.html");
+  res.sendFile(__dirname + "/Public/MessaginClient.html");
 });
 
 io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  socket.on("main Chat", (msg) => {
+    io.emit("main Chat", msg);
+    console.log(msg);
   });
 });
 
