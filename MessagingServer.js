696fc62561329e365f2a6d89173e97cca713625f
N.Tsa <24270865+HumAnTarg3t@users.noreply.github.com>
2022-12-17 13:13:44 +0100

    Rooms button

--- MessagingServer.js
+++ MessagingServer.js
@@ -11,8 +11,8 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   // Lytter på alle meldinger som kommer til "main Chat"
   socket.on("main Chat", (msg, room) => {
-    console.log(msg);
-    console.log(room);
+    console.log(`Message: ${msg}`);
+    console.log(`Room: ${room}`);
     // Om "room" ikke er med i meldingen fra klinet, send melding til alle som lytter på mainchat
     if (room == null) {
       // socket.broadcast.emit sender ikke melding tilbake til den som sendte meldingen først
@@ -20,24 +20,32 @@ io.on("connection", (socket) => {
       console.log("Melding sendt i Main Chat");
       // om melding inneholder en room så send melding til kun det roomet
     } else {
-      socket.to(room).emit("main Chat", msg);
+      socket.to(room).emit("main Chat", msg, room);
       console.log(`Melding sendt i ${room}`);
     }
   });
   socket.on("Room-Joined", (room, id) => {
     socket.join(room);
     console.log(`${id} joined ${room}`);
+    console.log(socket.rooms);
   });
-  // socket.on("Room-disconnect", (room, id) => {
-  //   socket.disconnect(room, true);
-  //   console.log(room);
-  // });
-  socket.on("Leave-Room", (room) => {
+
+  socket.on("Leave-Room", (room, id) => {
     socket.leave(room);
-    console.log(2 + room);
+    console.log(`${id} disconnected from: ${room}`);
+    console.log(socket.rooms);
+  });
+
+  socket.on("get-rooms", () => {
+    let arrayOfRooms = [];
+    const getRoomForUser = socket.rooms;
+    for (const entry of getRoomForUser.entries()) {
+      arrayOfRooms.push(entry);
+    }
+    console.log(getRoomForUser.set);
+    socket.emit("get-rooms-user", arrayOfRooms);
   });
 });
-
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
