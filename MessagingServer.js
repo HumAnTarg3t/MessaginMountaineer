4d48771dbe66abc47af16c0f2386cc06b9713edb
N.Tsa <24270865+HumAnTarg3t@users.noreply.github.com>
2022-12-19 19:46:35 +0100

    Bug fixes, changed some vars

--- MessagingServer.js
+++ MessagingServer.js
@@ -9,41 +9,46 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  // Lytter på alle meldinger som kommer til "main Chat"
-  socket.on("main Chat", (msg, room) => {
+  // Lytter på alle meldinger som kommer til "mainRoomToSend"
+  socket.on("mainRoomToSend", (msg, room) => {
     console.log(`Message: ${msg}`);
     console.log(`Room: ${room}`);
-    // Om "room" ikke er med i meldingen fra klinet, send melding til alle som lytter på mainchat
+    // Om "room" ikke er med i meldingen fra klinet, send melding til alle som lytter på mainRoomToSend
     if (room == null) {
       // socket.broadcast.emit sender ikke melding tilbake til den som sendte meldingen først
-      socket.broadcast.emit("main Chat", msg);
-      console.log("Melding sendt i Main Chat");
+      socket.broadcast.emit("mainRoomToReceive", msg);
+      console.log("Melding sendt i mainRoomToSend");
       // om melding inneholder en room så send melding til kun det roomet
     } else {
-      socket.to(room).emit("main Chat", msg, room);
+      socket.to(room).emit("mainRoomToReceive", msg, room);
       console.log(`Melding sendt i ${room}`);
     }
   });
+
+  // Legger til en bruker i ønsket room
   socket.on("Room-Joined", (room, id) => {
     socket.join(room);
     console.log(`${id} joined ${room}`);
     // console.log(socket.rooms);
   });
 
+  // Fjerner bruker fra ønsket room
   socket.on("Leave-Room", (room, id) => {
     socket.leave(room);
     console.log(`${id} disconnected from: ${room}`);
     // console.log(socket.rooms);
   });
 
-  socket.on("get-rooms", () => {
+  // Sender info til bruker om hvilket rooms den er i
+  socket.on("get-rooms", (showMsg) => {
     let arrayOfRooms = [];
     const getRoomForUser = socket.rooms;
     for (const entry of getRoomForUser) {
       arrayOfRooms.push(entry);
     }
     console.log(arrayOfRooms);
-    socket.emit("get-rooms-user", arrayOfRooms);
+    socket.emit("get-rooms-user", arrayOfRooms, showMsg);
+    console.log(showMsg);
   });
 });
 http.listen(port, () => {
