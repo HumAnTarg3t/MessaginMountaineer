f3ec3cd189bb21dcd4381282c7ed81ed66171e70
N.Tsa <24270865+HumAnTarg3t@users.noreply.github.com>
2023-01-14 11:53:09 +0100

    flyttet post mottaket fra app.js til loginexpress

--- LoginExpress.js
+++ LoginExpress.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv").config();
 const proxy = require("express-http-proxy");
+const cors = require("cors");
 const express = require("express");
 const morgan = require("morgan");
 const app = require("express")();
@@ -17,6 +18,41 @@ app.get("/Login", (req, res) => {
   res.sendFile(__dirname + "/public2/LoginClient.html");
   //res.sendFile("/Public2/LoginClient.html");
 });
+
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: ["http://localhost:3002"],
+    methods: "POST",
+  })
+);
+app.use(express.json({ type: "application/json" }));
+
+const compareArrays = (a, b) => {
+  return JSON.stringify(a) === JSON.stringify(b);
+};
+
+app.post("/Api/v1/Post", async (req, res) => {
+  const data = await req.body;
+  let jsonBodyKeys = Object.keys(data);
+  const reqKeyForRequest = ["Username", "pwd"];
+  // console.log(`Keys from post: ${jsonBodyKeys}`);
+  // console.log(req.headers);
+  // console.log(`Keys required for post: ${reqKeyForRequest}`);
+  if (
+    req.headers["content-type"] == "application/json" &&
+    compareArrays(jsonBodyKeys, reqKeyForRequest)
+  ) {
+    // readFromDB(process.env.mongoDB_Client_dev, "UsersTable").catch(console.dir);
+    console.log(data);
+    res.send({ heh: "wqe" });
+  } else {
+    res.statusCode = 400;
+    res.send("content-type feil, forventer application/json");
+    console.log(req.headers);
+  }
+});
+
 //console.log(process.env.DOMAIN_NAME)
 http.listen(port, () => {
   console.log(`Express login server running at http://localhost:${port}/`);
--- app.js
+++ app.js
@@ -11,43 +11,43 @@ const { randomUUID } = require("crypto");
 let csrfToken = randomUUID();
 // console.log(csrfToken);
 
-app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: ["http://localhost:3002"],
-    methods: "POST",
-  })
-);
-app.use(express.json({ type: "application/json" }));
+// app.use(morgan("dev"));
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3002"],
+//     methods: "POST",
+//   })
+// );
+// app.use(express.json({ type: "application/json" }));
 
-const compareArrays = (a, b) => {
-  return JSON.stringify(a) === JSON.stringify(b);
-};
+// const compareArrays = (a, b) => {
+//   return JSON.stringify(a) === JSON.stringify(b);
+// };
 
 app.get("/", (req, res) => {
   res.redirect("http://localhost:3002/login");
 });
 
-app.post("/Api/v1/Post", async (req, res) => {
-  const data = await req.body;
-  let jsonBodyKeys = Object.keys(data);
-  const reqKeyForRequest = ["Username", "pwd"];
-  // console.log(`Keys from post: ${jsonBodyKeys}`);
-  // console.log(req.headers);
-  // console.log(`Keys required for post: ${reqKeyForRequest}`);
-  if (
-    req.headers["content-type"] == "application/json" &&
-    compareArrays(jsonBodyKeys, reqKeyForRequest)
-  ) {
-    // readFromDB(process.env.mongoDB_Client_dev, "UsersTable").catch(console.dir);
-    console.log(data);
-    res.send({ heh: "wqe" });
-  } else {
-    res.statusCode = 400;
-    res.send("content-type feil, forventer application/json");
-    console.log(req.headers);
-  }
-});
+// app.post("/Api/v1/Post", async (req, res) => {
+//   const data = await req.body;
+//   let jsonBodyKeys = Object.keys(data);
+//   const reqKeyForRequest = ["Username", "pwd"];
+//   // console.log(`Keys from post: ${jsonBodyKeys}`);
+//   // console.log(req.headers);
+//   // console.log(`Keys required for post: ${reqKeyForRequest}`);
+//   if (
+//     req.headers["content-type"] == "application/json" &&
+//     compareArrays(jsonBodyKeys, reqKeyForRequest)
+//   ) {
+//     // readFromDB(process.env.mongoDB_Client_dev, "UsersTable").catch(console.dir);
+//     console.log(data);
+//     res.send({ heh: "wqe" });
+//   } else {
+//     res.statusCode = 400;
+//     res.send("content-type feil, forventer application/json");
+//     console.log(req.headers);
+//   }
+// });
 
 http.listen(port, () => {
   console.log(`App login server running at http://localhost:${port}/`);
