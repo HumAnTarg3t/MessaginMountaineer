cfc1135f5cfce7af245bb81e6ce0d4260730ed4c
N.Tsa <24270865+HumAnTarg3t@users.noreply.github.com>
2023-01-18 22:58:17 +0100

    username and passord check, formatert code

--- LoginExpress.js
+++ LoginExpress.js
@@ -15,10 +15,22 @@ app.use(
     methods: "POST",
   })
 );
-app.use(express.json({ type: "application/json" }));
+app.use(
+  express.json({
+    type: "application/json",
+  })
+);
 
 const compareArrays = (a, b) => {
   return JSON.stringify(a) === JSON.stringify(b);
+};
+
+const defaultParamChecker = (reqHeaders, jsonBodyKeys, reqKeyForRequest) => {
+  if (reqHeaders == "application/json" && compareArrays(jsonBodyKeys, reqKeyForRequest)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 app.get("/Login", (req, res) => {
@@ -29,64 +41,56 @@ app.post("/Api/v1/Post/Login", async (req, res) => {
   const data = await req.body;
   let jsonBodyKeys = Object.keys(data);
   const reqKeyForRequest = ["Username", "pwd"];
-  if (
-    req.headers["content-type"] == "application/json" &&
-    compareArrays(jsonBodyKeys, reqKeyForRequest)
-  ) {
+
+  if (defaultParamChecker(req.headers["content-type"], jsonBodyKeys, reqKeyForRequest)) {
     readFromDB(process.env.mongoDB_Client_dev, "UsersTable", {
       UserName: data.Username,
     }).then((responseFromDB) => {
-      if (responseFromDB) {
-        res.send("User found");
+      if (responseFromDB && comparehash(data.pwd, responseFromDB[0].pwd)) {
+        res.send({ response: "User found" });
         // redirect til messagingclient med AUTH (auth er ikke på plass ennå)
       } else {
         // redirect til setup page
-        res.send("User not found");
+        res.send({ response: "User not found/wrong username or password" });
       }
     });
-    console.log(data);
   } else {
     res.statusCode = 400;
-    res.send("content-type feil, forventer application/json");
+    res.send({ response: "content-type feil, forventer application/json" });
     console.log(req.headers);
   }
 });
 
+//  ------------------------------------------------------------------------------------------ REGISTER ------------------------------------------------------------------------------------------
 app.post("/Api/v1/Post/Register", async (req, res) => {
   const data = await req.body;
   const reqKeyForRequest = ["Register_Login", "Register_pwd", "Register_Email"];
   let jsonBodyKeys = Object.keys(data);
-  let hashedRegister_pwd = await hashSomething(data.Register_pwd);
   const date = new Date();
-  let doesUserexist = await readFromDB(
-    process.env.mongoDB_Client_dev,
-    "UsersTable",
-    {
-      UserName: data.Username,
-    }
-  );
+  let doesUserexist = await readFromDB(process.env.mongoDB_Client_dev, "UsersTable", {
+    UserName: data.Register_Login,
+  });
 
-  console.log(data);
-  console.log(doesUserexist);
-  if (
-    req.headers["content-type"] == "application/json" &&
-    compareArrays(jsonBodyKeys, reqKeyForRequest) &&
-    doesUserexist === "false"
-  ) {
-    //send hashed info til DB og redirect til chat med auth
+  if (defaultParamChecker(req.headers["content-type"], jsonBodyKeys, reqKeyForRequest) && !doesUserexist) {
+    let hashedRegister_pwd = await hashSomething(data.Register_pwd);
     await writeToDB(process.env.mongoDB_Client_dev, "UsersTable", {
       UserName: data.Register_Login,
       pwd: hashedRegister_pwd,
       mail: data.Register_Email,
       ActiveStatus: true,
       AccountCreated: date,
-      RoomId: "Double(qweqweqwe.0)",
+      RoomId: "",
     });
-    console.log(hashedRegister_pwd);
-    res.send(hashedRegister_pwd);
+    res.send({ response: "User created" });
+  } else if (
+    defaultParamChecker(req.headers["content-type"], jsonBodyKeys, reqKeyForRequest) &&
+    doesUserexist !== "false"
+  ) {
+    res.statusCode = 400;
+    res.send({ response: "User exists" });
   } else {
     res.statusCode = 400;
-    res.send("Bad req");
+    res.send({ response: "Bad req" });
   }
 });
 
